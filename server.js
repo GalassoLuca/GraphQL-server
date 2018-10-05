f2d88e9e38863d32930be42089b79d4dfce47c82
Luca Galasso <luca@WonderLuca.local>
2018-10-05 12:23:13 +0200

    feat(eslint): added .eslintrc and fix base on it

--- server.js
+++ server.js
@@ -4,61 +4,61 @@ const { buildSchema } = require('graphql');
 const app = express();
 
 const coursesData = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
+  {
+    id: 1,
+    title: 'The Complete Node.js Developer Course',
+    author: 'Andrew Mead, Rob Percival',
+    description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+    topic: 'Node.js',
+    url: 'https://codingthesmartway.com/courses/nodejs/'
+  },
+  {
+    id: 2,
+    title: 'Node.js, Express & MongoDB Dev to Deployment',
+    author: 'Brad Traversy',
+    description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+    topic: 'Node.js',
+    url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+  },
+  {
+    id: 3,
+    title: 'JavaScript: Understanding The Weird Parts',
+    author: 'Anthony Alicea',
+    description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+    topic: 'JavaScript',
+    url: 'https://codingthesmartway.com/courses/understand-javascript/'
+  }
 ]
 const getCourse = function (args) {
-    const id = args.id;
-    return coursesData.filter(course => {
-        return course.id == id;
-    })[0];
+  const id = args.id;
+  return coursesData.filter(course => {
+    return course.id == id;
+  })[0];
 }
 const getCourses = function (args) {
-    if (args.topic) {
-        const topic = args.topic;
-        return coursesData.filter(course => course.topic === topic);
-    } else {
-        return coursesData;
-    }
+  if (args.topic) {
+    const topic = args.topic;
+    return coursesData.filter(course => course.topic === topic);
+  } else {
+    return coursesData;
+  }
 }
 const updateCourseTopic = function ({ id, topic }) {
-    coursesData.map(course => {
-        if (course.id === id) {
-            course.topic = topic;
-            return course;
-        }
-    });
-    return coursesData.filter(course => course.id === id)[0];
+  coursesData.map(course => {
+    if (course.id === id) {
+      course.topic = topic;
+      return course;
+    }
+  });
+  return coursesData.filter(course => course.id === id)[0];
 }
 
 /**
  * Create an express server and a GraphQL endpoint
  */
 app.use('/graphql', express_graphql({
-    // GraphQL schema
-    schema: buildSchema(`
+  // GraphQL schema
+  schema: buildSchema(`
         type Query {
             course(id: Int!): Course
             courses(topic: String): [Course]
@@ -75,12 +75,12 @@ app.use('/graphql', express_graphql({
             url: String
         }
     `),
-    rootValue: {
-        course: getCourse,
-        courses: getCourses,
-        updateCourseTopic: updateCourseTopic
-    },
-    graphiql: true
+  rootValue: {
+    course: getCourse,
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
+  },
+  graphiql: true
 }));
 
 /**
@@ -88,22 +88,22 @@ app.use('/graphql', express_graphql({
  */
 // A GET to the root of a resource returns a list of that resource
 app.get('/api/', function (req, res) {
-    return res.json(getCourses({}));
+  return res.json(getCourses({}));
 })
 
 // We specify a param in our path for the GET of a specific object
 app.get('/api/:id', function (req, res) {
-    return res.json(getCourse(req.params));
+  return res.json(getCourse(req.params));
 })
 
 // Similar to the GET on an object, to update it we can PATCH
 app.post('/api/:id', function (req, res) {
-    // TODO get the topic from the request with body-parser
-    req.params.topic = "Node Node Node"
-    return res.json(updateCourseTopic(req.params));
+  // TODO get the topic from the request with body-parser
+  req.params.topic = "Node Node Node"
+  return res.json(updateCourseTopic(req.params));
 })
 
 app.listen(4000, () => {
-    console.log('Express GraphQL Server Now Running On localhost:4000/graphql')
-    console.log('API REST Server Now Running On localhost:4000/api')
+  console.log('Express GraphQL Server Now Running On localhost:4000/graphql')
+  console.log('API REST Server Now Running On localhost:4000/api')
 })
